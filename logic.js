13d9d80e433f13f38d70d02ac9471903092d3227
karopkds <karopdezosa541999@gmail.com>
2024-01-29 22:50:43 +0530

    third commit

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 let point = 10;
+let run = 0;
 function submit01() {
   let enterNumber = parseInt(document.getElementById('NumberEntred').value);
   let random = Math.floor(Math.random() * 10);
@@ -7,12 +8,19 @@ function submit01() {
     if (gameOver === true) {
       location.reload();
     }
+  } else if (run == 5) {
+    let youwin = confirm('YOU WON!!!!!');
+    if (youwin === true) {
+      location.reload();
+    }
   } else if (enterNumber != random) {
     point = point - 1;
   } else if (enterNumber == random) {
-    point = point + 1;
+    run = run + 1;
   }
   document.getElementById('score').innerText = point;
+  document.getElementById('run').innerText = run;
+  document.getElementById('randomnumber').innerText = random;
   //console.log(enterNumber);
   //console.log(random);
   //console.log(`###### ${point}`);
